6ce681b69b01aa3eff5a53c36aff169b729e5016
AlexD <alexandredesoutter.pro@gmail.com>
2021-09-10 11:56:18 +0200

    ajout de nodemon et npm init

---
+++ README.md
@@ -0,0 +1,9 @@
+Pour commencer la Base de données en Node JS : 
+
+installer Node JS via le site internet : V16.2.0
+
+1. npm install sequelize
+2. npm install mariadb
+3. npm intall express
+4. npm install dotenv
+5. npm install nodemon
--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = express();
 const sequelize = require('./models');
 const PORT = process.env.PORT || 5000;
 
+// // Création de l'API
+// app.get('/products', (req, res) => {
+//     res.send({
+//         id: 1,
+//         catégory: "shoulder",
+//         name: "scaled"
+//     })
+// });
+
 // Configurer notre serveur pour utiliser ces routeurs
 const userRouter = require('./routers/userRouter');
 app.use('/user', userRouter);
@@ -31,8 +40,9 @@ console.log('Checking Database connection...');
 
 sequelize.authenticate()
 .then(() => {
-    console.log("Database connection OK!");
-    // Synchronise la base de données avec nos modèles
+    // console.log("Database connection OK!");
+    // // Synchronise la base de données avec nos modèles
+    // // Travailler sur la construction des bases de données
     // sequelize.sync({force:true})
 
     app.listen(PORT, () => {
