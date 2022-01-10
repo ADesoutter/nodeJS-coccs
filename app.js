9eac683f931389cfa488da7c09fb58399e7fdf90
AlexD <alexandredesoutter.pro@gmail.com>
2022-01-10 21:51:33 +0100

    modif de l'app.js

--- app.js
+++ app.js
@@ -3,26 +3,6 @@ const app = express();
 const sequelize = require('./models');
 const PORT = process.env.PORT || 3080;
 
-// Configurer notre serveur pour utiliser ces routeurs
-const usersRouter = require('./routers/usersRouter');
-app.use('/users', usersRouter);
-
-const productsRouter = require('./routers/productsRouter');
-app.use('/products', productsRouter);
-
-const categoriesRouter = require('./routers/categoriesRouter');
-app.use('/categories', categoriesRouter);
-
-const estimatesRouter = require('./routers/estimatesRouter');
-app.use('/estimates', estimatesRouter);
-
-const contactsRouter = require('./routers/contactsRouter');
-app.use('/contacts', contactsRouter);
-
-const faqsRouter = require('./routers/faqsRouter');
-app.use('/faqs', faqsRouter);
-
-// Je veux accepter du JSON en envoi d'informations
 app.use(express.json());
 
 console.log('Checking Database connection...');
